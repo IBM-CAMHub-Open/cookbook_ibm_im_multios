01900cb132da33f3dab339d5e1064ae87c04458e
CAMHub PublishBot <travis@example.org>
2017-10-17 21:22:44 +0000

    Automated CAMHub::publish on 2017-10-17 21:22:44 UTC

--- chef/cookbooks/im/metadata.rb
+++ chef/cookbooks/im/metadata.rb
@@ -296,7 +296,7 @@ Response file template:
 <preference name='com.ibm.cic.common.core.preferences.eclipseCache' value='<%= @IMSHARED %>'/>
 ```
 EOH
-version '0.1.29'
+version '1.0.0'
 
 recipe 'im::cleanup.rb', '
 Cleanup recipe ( cleanup.rb )
